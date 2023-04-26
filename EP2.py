4addfa6d6655bde5eab50e56172d6fc30adf786c
victorfariasoares <victorfs2@al.insper.edu.br>
2023-04-26 08:01:38 -0300

    Exercício 1 - Define Posições finalizado

--- EP2.py
+++ EP2.py
@@ -8,16 +8,14 @@ def define_posicoes(linha, coluna, orientacao, tamanho):
     posicoes[0].insert(0,linha)
     posicoes[0].insert(1,coluna)
     if orientacao == 'vertical':
-        for e in range(len(posicoes)):
-            posicoes[e+1].insert(1,coluna)
-            posicoes[e+1].insert(0, linha + soma_linha)
+        for e in range(1, len(posicoes)):
+            posicoes[e].insert(1,coluna)
+            posicoes[e].insert(0, linha + soma_linha)
             soma_linha += 1
     if orientacao == 'horizontal':
-        for e in range(len(posicoes)):
-            posicoes[e+1].insert(0,linha)
-            posicoes[e+1].insert(1, coluna + soma_coluna)
+        for e in range(1, len(posicoes)):
+            posicoes[e].insert(0,linha)
+            posicoes[e].insert(1, coluna + soma_coluna)
             soma_coluna += 1
     return posicoes
 
-x = define_posicoes(2, 4, 'vertical', 3)
-print(x)
